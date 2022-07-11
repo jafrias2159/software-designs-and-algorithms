c763faa3d18130a63cc45d73b25a5f7aa5108d68
Jorge Frias <Jorge_Frias@epam.com>
2022-07-11 13:41:08 -0500

    Adding files to home task 2

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Item.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Item.ts
@@ -1,11 +1,62 @@
 import { Comparable } from './Comparable';
 
 let id = 0;
-
+let counter = 0;
 export abstract class Item implements Comparable<Item> {
+   
     public compareTo(other: Item): number {
         // your code goes here
+        if(this.value > other.value) return 1;
+        if(this.value < other.value) return 1;
+        return this.name.localeCompare(other.name)
     }
 
     // your code goes here
+    constructor(name: string, value: number, weigth: number){
+        this.name = name,
+        this.value = value;
+        this.weigth = weigth;
+        this.id = id;
+        id++;
+    }
+
+    private _id: number;
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    private _value: number;
+    public get value(): number {
+        return this._value;
+    }
+    public set value(value: number) {
+        this._value = value;
+    }
+    private _name: string;
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+    private _weigth: number;
+    public get weigth(): number {
+        return this._weigth;
+    }
+    public set weigth(value: number) {
+        this._weigth = value;
+    }
+
+    public use(){}
+    abstract toString(): string;    
+    static  get getnumberOfItems(): number{
+        return counter;
+    }
+
+    static reset(){
+        counter = 0;
+    }
 }
--- docs/3-building-blocks-of-oop-part-2/hometask/src/Sword.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Sword.ts
@@ -1,1 +1,13 @@
-// your code goes here
+import { Weapon } from "./Weapon";
+
+export class Sword extends Weapon {
+  constructor(baseDamage: number, baseDurability: number, value: number) {
+    super("sword", baseDamage, baseDurability, value);
+  }
+  public polish() {
+    const maximunValue = parseInt(((25 / 100) * this.baseDamage).toFixed(2));
+    if (this.damageModifier + this.MODIFIER_CHANGE_RATE < maximunValue) {
+      this.damageModifier += this.MODIFIER_CHANGE_RATE;
+    }
+  }
+}
