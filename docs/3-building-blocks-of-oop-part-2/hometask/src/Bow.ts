e8f982c39d3ebd7d05a688181e45930cce41ddbb
Jorge Frias <Jorge_Frias@epam.com>
2022-07-11 14:04:24 -0500

    Adding bow & inventory

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Bow.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Bow.ts
@@ -1,1 +1,14 @@
-// your code goes here
+import { Weapon } from "./Weapon";
+
+export class Bow extends Weapon {
+  
+    constructor(baseDamage: number, baseDurability: number, value: number) {
+    super("bow", baseDamage, baseDurability, value);
+  }
+
+  public polish() {
+    if (this.getDurability() <= 1) {
+      this.durabilityModifier = +this.MODIFIER_CHANGE_RATE;
+    }
+  }
+}
--- docs/3-building-blocks-of-oop-part-2/hometask/src/Inventory.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Inventory.ts
@@ -1,1 +1,28 @@
-// your code goes here
+import { Item } from "./Item";
+import { ItemComparator } from "./ItemComparator";
+
+export class Invertory implements ItemComparator {
+  compare(first: Item, second: Item): number {
+    throw new Error("Method not implemented.");
+  }
+  private items: Item[];
+
+  public addItem(item: Item) {
+    this.items.push(item);
+  }
+
+  public sort():void;
+  public sort(comparator: ItemComparator): void;
+  public sort(comparator?: ItemComparator): void {
+    if (typeof comparator === "undefined") {
+      // Sort by any characteritc
+    } else {
+      //Sort stuff with ItemWeightComparator class
+      //new ItemWeightComparator()
+    }
+  }
+
+  public toString(){
+    return this.items.join(',');
+  }
+}
